6ae175d313593b9d211302e7d547d18c6a622496
apichat-h <139953915+apichat-h@users.noreply.github.com>
2023-10-05 13:52:19 +0700

    Update page.tsx

--- app/encode/page.tsx
+++ app/encode/page.tsx
@@ -272,7 +272,7 @@ function Encode() {
                 {encodedImage && (
                   <div className=" flex h-[100%] items-center justify-center">
                     <img
-                      src={resizeImage(encodedImage,test,300,300)}
+                      src={encodedImage}
                       alt="Encoded Image"
                       className=" object-scale-down max-w-[95%] max-h-[95%] border border-red-400"
                     />
