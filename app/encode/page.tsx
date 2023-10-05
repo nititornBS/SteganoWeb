2f1282fdff1fe2c3900b6d7119f2841229cb4352
apichat-h <139953915+apichat-h@users.noreply.github.com>
2023-10-05 13:50:43 +0700

    Update page.tsx

--- app/encode/page.tsx
+++ app/encode/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { Spinner } from "@nextui-org/react";
-// import resizeImage from "./resizeimage";
+import resizeImage from "./resizeimage";
 
 function Encode() {
   const [selectedCoverImage, setSelectedCoverImage] = useState(null);
@@ -272,7 +272,7 @@ function Encode() {
                 {encodedImage && (
                   <div className=" flex h-[100%] items-center justify-center">
                     <img
-                      src={encodedImage}
+                      src={resizeImage(encodedImage,test,300,300)}
                       alt="Encoded Image"
                       className=" object-scale-down max-w-[95%] max-h-[95%] border border-red-400"
                     />
