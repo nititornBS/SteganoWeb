579cf7450fa55399888ed5641ead59fc369d6a1a
apichat-h <139953915+apichat-h@users.noreply.github.com>
2023-10-05 12:12:12 +0700

    Update page.tsx

--- app/encode/page.tsx
+++ app/encode/page.tsx
@@ -271,21 +271,10 @@ function Encode() {
 
                 {encodedImage && (
                   <div className=" flex h-[100%] items-center justify-center">
-                    <style> 
-                    .pixelated {
-                      image-rendering: pixelated;
-                    }
-                    .smooth {
-                      image-rendering: smooth;
-                    }
-                    .high-quality {
-                      image-rendering: high-quality;
-                    }
-                    </style>
                     <img
                       src={encodedImage}
                       alt="Encoded Image"
-                      className=" image smooth max-w-[95%] max-h-[95%] border border-red-400   "
+                      className=" object-scale-down max-w-[95%] max-h-[95%] border border-red-400   "
                     />
                   </div>
                 )}
